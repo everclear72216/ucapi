13f777e5d49775f2e4f098b8ccd38a7baaf0f418
Martin Bertsche <martin72216@googlemail.com>
2019-03-04 17:37:17 +0100

    startup: Added API for clock initialization
    closes #1

---
+++ include/ucapi/clock.h
@@ -0,0 +1,10 @@
+#ifndef UCAPI_CLOCK_H_INCLUDED
+#define UCAPI_CLOCK_H_INCLUDED
+
+/**
+ * @brief      Initializes the system clocks according to the build
+ *             configuration.
+ */
+extern void ucapi_clock_init(void);
+
+#endif /* UCAPI_CLOCK_H_INCLUDED */
